ffbb151dd06b0af7c6e417b61cd3d9f83bcdb607
NickMonks <nicolasmg94@gmail.com>
2025-01-17 00:10:35 +0000

    add client retries

--- src/Infrastructure/PaymentGateway.ApiClient/ApiClientServiceRegistration.cs
+++ src/Infrastructure/PaymentGateway.ApiClient/ApiClientServiceRegistration.cs
@@ -6,6 +6,9 @@ using PaymentGateway.Application.Contracts.ApiClient;
 using PaymentGateway.Infrastructure.ApiClient;
 using PaymentGateway.Infrastructure.Handlers;
 
+using Polly;
+using Polly.Extensions.Http;
+
 namespace PaymentGateway.Infrastructure;
 
 public static class ApiClientServiceRegistration
@@ -21,9 +24,28 @@ public static class ApiClientServiceRegistration
 
                 client.BaseAddress = new Uri(simulatorApiSettings.BaseUri);
             })
+            .AddPolicyHandler(GetRetryPolicy())
             .AddHttpMessageHandler<ApiExceptionHandler>();
         services.AddTransient<ApiExceptionHandler>();
         return services;
+    }
+    
+    /// <summary>
+    /// Retry policy for our HTTP Client. We will retry on 5xx, 408 (Timeout) errors.
+    /// The current configuration has 5 retires with an exponential backoff strategy.
+    /// </summary>
+    /// <returns></returns>
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError() 
+            .WaitAndRetryAsync(
+                retryCount: 5, 
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (outcome, timespan, retryAttempt, context) =>
+                {
+                    Console.WriteLine($"Retrying... Attempt {retryAttempt}. Waiting {timespan}.");
+                });
     }
 
 }
--- test/PaymentGateway.Api.UnitTests/ApiClient/SimulatorApiClientTests.cs
+++ test/PaymentGateway.Api.UnitTests/ApiClient/SimulatorApiClientTests.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 
 using Microsoft.Extensions.Logging;
 
+using PaymentGateway.Application.Contracts.ApiClient;
 using PaymentGateway.Application.Exceptions;
 using PaymentGateway.Infrastructure.ApiClient;
 using PaymentGateway.Infrastructure.Handlers;
